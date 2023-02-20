f0cafab60b5137d02503a99f9f95fbf67b989af8
Gass <gassma803@gmail.com>
2023-02-20 11:32:43 +0100

    add Product metier

--- src/main/java/dao/ProductDao.java
+++ src/main/java/dao/ProductDao.java
@@ -1,4 +1,33 @@
 package dao;
 
+import entities.Product;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class ProductDao {
+    private  List<Product> products = new ArrayList<Product>();
+    public Product addProduct(Product product) throws Exception{
+        products.add(product);
+        return product;
+    }
+
+    public Product updateProduct(final Product product){
+        Product prod=products
+                .stream()
+                .filter(p -> p.getCode() == product.getCode())
+                .findFirst().
+                orElseThrow(() ->new RuntimeException("product not found"));
+        prod.setLabel(product.getLabel());
+        prod.setPrice(product.getPrice());
+        prod.setQuantity(product.getQuantity());
+        return product;
+    }
+
+    public List<Product> getProducts() throws Exception{
+        return products;
+    }
+
+
+
 }
