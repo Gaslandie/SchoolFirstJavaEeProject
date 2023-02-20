4fddfdb66a96d6af30f873a9315ad97d31adff89
Gass <gassma803@gmail.com>
2023-02-20 10:29:33 +0100

    ajout de product

--- src/main/java/entities/Product.java
+++ src/main/java/entities/Product.java
@@ -1,4 +1,11 @@
 package entities;
 
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class Product {
+    private Long code;
+    private String label;
+    private double price;
+    private int quantity;
 }
